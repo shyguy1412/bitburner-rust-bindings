68769b9497535b0f6158c85c77344bf36258adb9
Shy <shyguyx1412@gmail.com>
2025-03-30 19:28:28 +0000

    be less stupid about string concatination

--- src/macros/src/lib.rs
+++ src/macros/src/lib.rs
@@ -8,8 +8,8 @@ pub fn bb_bindgen(_: TokenStream, input: TokenStream) -> TokenStream {
     let mut body = parse_macro_input!(input as syn::ItemFn);
 
     let og_ident = body.sig.ident.clone();
-    let fn_ident = &("__bb_bind_".to_owned() + &body.sig.ident.to_string());
-    body.sig.ident = syn::Ident::new(fn_ident, Span::call_site().into());
+    let fn_ident = format!("__bb_bind_{}", body.sig.ident);
+    body.sig.ident = syn::Ident::new(fn_ident.as_str(), Span::call_site().into());
     let fn_ident = body.sig.ident.clone();
 
     quote::quote! {
